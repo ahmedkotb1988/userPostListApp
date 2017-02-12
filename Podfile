567e0846ae88bc2718b934d855d88f0aaca5b9e8
Ahmed kotb <ahmedkotb@Vulletts-Mac-mini.local>
2017-02-12 22:37:36 +0200

    pod Push

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'userPostListApp' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for userPostListApp
+    pod 'Alamofire', '~> 4.3'
+    pod 'SVProgressHUD'
+  target 'userPostListAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'userPostListAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- PostsViewController.swift
+++ PostsViewController.swift
@@ -7,21 +7,82 @@
 //
 
 import UIKit
+import SVProgressHUD
 
-class PostsViewController: UIViewController {
+class PostsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, webServiceManagerDelegate{
 
+    @IBOutlet weak var postsTableView: UITableView!
+    
+    var userID : NSInteger? // passed from users' view
+    var postList = [Post]() // Will be populated from the web service
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.postsTableView.isHidden = true
+        SVProgressHUD.show()
+        let serviceManager = webServiceManager()
+        serviceManager.delegate = self
+        
+        serviceManager.getPostsForUserWithID(ID: self.userID!)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        self.navigationItem.title = "Posts"
+    }
+    
+   
+    
+    // MARK:- Web Service Manager Delegate
+    
+    func didFinishRetrieving(sender: webServiceManager, users: [User]?, Posts: [Post]?) {
+        
+        self.postList = Posts!
+        self.postsTableView.reloadData()
+        SVProgressHUD.dismiss()
+        self.postsTableView.isHidden = false
+    }
+    
+    // MARK:- TableView Datasource and Delegate
+    
+    func  numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return postList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell : PostsTableViewCell = tableView.dequeueReusableCell(withIdentifier: "postsCell", for: indexPath) as! PostsTableViewCell
+        
+        let userPost : Post = postList[indexPath.row]
+        
+        cell.populateCellWithData(title: userPost.title!, body: userPost.body!)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return UITableViewAutomaticDimension
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
     /*
     // MARK: - Navigation
 
