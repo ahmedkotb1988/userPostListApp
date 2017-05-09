b6670cdf16ebc5bb5b77d1319b0b370c22fd062c
Mohamed Alsadek <mohamed.alsadek@runtastic.com>
2017-05-09 18:26:50 +0200

    Removing files

--- Pods/Target Support Files/Pods-userPostListApp/Pods-userPostListApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_userPostListApp : NSObject
-@end
-@implementation PodsDummy_Pods_userPostListApp
-@end
--- Pods/Target Support Files/Pods-userPostListAppUITests/Pods-userPostListAppUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_userPostListAppUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_userPostListAppUITestsVersionString[];
-
